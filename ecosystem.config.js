a3036ee594fe7bcc95ad18469fc81c1cebd56bf2
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-09-23 15:38:28 +0300

    fix: ecosystem configuration change, add key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ const {
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF,
-  REPO,
+  DEPLOY_REPO,
+  DEPLOY_SSH_KEY,
 } = process.env;
 
 module.exports = {
@@ -16,7 +17,7 @@ module.exports = {
     },
     {
       name: 'mesto-frontend',
-      script: 'npx serve -s build', 
+      script: 'npx serve -s build',
       cwd: './frontend',
     },
   ],
@@ -26,13 +27,12 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: REPO,
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-
-      'pre-deploy': `mkdir -p ${DEPLOY_PATH}/shared && scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env`,
-
+      key: DEPLOY_SSH_KEY,
+      'pre-deploy': `scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env`,
       'post-deploy': `
-        cd backend && npm install && 
+        cd backend && npm install &&
         cd ../frontend && npm install && npm run build &&
         pm2 reload ecosystem.config.js --env production
       `,
