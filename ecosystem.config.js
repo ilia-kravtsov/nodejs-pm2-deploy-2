ae86d3bb90a36c74769d1c606bda35330b993a25
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-09-23 15:05:38 +0300

    fix: change deploy configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,28 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, 
+  DEPLOY_HOST, 
+  DEPLOY_PATH, 
+  DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO
+} = process.env;
+
 module.exports = {
   apps: [{
     name: 'backend',
     script: './dist/app.js',
-  }, {
-    name: 'frontend',
-    script: 'serve',
-    args: ['-s', 'build', '-l', '3001'],
-    cwd: 'frontend'
   }],
 
   deploy: {
     production: {
-      user: 'user',
-      host: '158.160.195.220', 
-      key: 'C:/Users/kravt/.ssh/vm_access/private_key',
-      ref: 'origin/master',
-      repo: 'git@github.com:ilia-kravtsov/nodejs-pm2-deploy-2.git',
-      path: '/home/user/app',
-      'pre-deploy': `scp backend/.env user@158.160.195.220:/home/user/app/shared/backend/.env`,
-      'post-deploy': `
-        ln -sf /home/user/app/shared/backend/.env /home/user/app/current/backend/.env &&
-        cd /home/user/app/current/backend && npm install && npm run build &&
-        cd ../frontend && npm install && npm run build &&
-        pm2 startOrRestart ecosystem.config.js --env production
-      `,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
