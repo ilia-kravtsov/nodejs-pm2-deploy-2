745224f400e3305b5c8b38a1e811972fdc37ba57
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-09-23 15:14:16 +0300

    fix: add ssh key to configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,6 +17,7 @@ module.exports = {
       ref: process.env.DEPLOY_REF || 'origin/master',
       repo: process.env.DEPLOY_REPO || 'git@github.com:ilia-kravtsov/nodejs-pm2-deploy-2.git', 
       path: process.env.DEPLOY_PATH || '/home/user/app',
+      key: process.env.SSH_KEY || 'C:/Users/kravt/.ssh/vm_access/private_key',
       'pre-deploy': `scp ./*.env ${process.env.DEPLOY_USER || 'user'}@${process.env.DEPLOY_HOST || '158.160.195.220'}:${process.env.DEPLOY_PATH || '/home/user/app'}`,
       'post-deploy': `
         cd backend && npm i && npm run build &&
