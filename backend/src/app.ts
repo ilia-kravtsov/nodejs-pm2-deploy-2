18df4b0b30600e1f4bf94286333ea9f760230e87
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-09-24 16:42:05 +0300

    feat: helmet and cors add

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,10 @@ import 'dotenv/config';
 
 import express from 'express';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -13,8 +14,18 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
+const allowedOrigins = [
+  'https://mesto.kravtsov.nomorepartiessbs.ru', 
+];
+
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true, // если используешь куки или авторизацию
+  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
